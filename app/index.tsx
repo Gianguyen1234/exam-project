a8fbf920af27242e050774ef6e4be0c98fdc2c52
Thao Nguyen <nguyenluthao252@gmail.com>
2024-12-11 08:53:15 +0700

    finish 2a_1

--- app/index.tsx
+++ app/index.tsx
@@ -3,14 +3,14 @@ import { View } from "react-native";
 import StartScreen from "./screens/StartScreen";
 
 import PopularProducts from "./screens/PopularProducts"; 
-// import ShowScreen from "./screens/ShowScreen"; 
+import ShowScreen from "./screens/ShowScreen"; 
 
 export default function Index() {
   return (
     <View style={{ flex: 1 }}>
       <StartScreen navigation={undefined} />
       <PopularProducts />
-      {/* <ShowScreen/> */}
+      <ShowScreen/>
     </View>
   );
 }
